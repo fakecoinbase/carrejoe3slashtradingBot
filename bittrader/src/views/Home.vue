715f3a8841a4f10aa8fc9e17d5d620ab88abeda0
Joe <carre.joe3@gmail.com>
2020-04-12 10:25:42 +0100

    renamed Hello World component to Price List and edited the app bar

--- bittrader/src/views/Home.vue
+++ bittrader/src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PriceList/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import PriceList from '@/components/PriceList.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    PriceList
   }
 }
 </script>
