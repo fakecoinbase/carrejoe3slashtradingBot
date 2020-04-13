2f256fe69144f08015f7b6c3597ebcbc1c945d69
Joe <carre.joe3@gmail.com>
2020-04-13 11:59:29 +0100

    get current price of BTC/USD on server start

--- server/index.js
+++ server/index.js
@@ -1,16 +1,23 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const coinbase = require('coinbase')
+
 const config = require('./config/config')
-const client = new coinbase.Client({ 'apiKey': config.COINBASE_API_KEY, 'apiSecret': config.COINBASE_API_SECRET })
+
+const coinbase = require('coinbase')
+const client = new coinbase.Client({
+    'apiKey': config.COINBASE_API_KEY,
+    'apiSecret': config.COINBASE_API_SECRET,
+    strictSSL: false
+})
+
 const app = express()
 
 // Middleware
 app.use(bodyParser.json())
 app.use(cors())
 
-const login = require('./routes/api/login')
+const login = require('./routes/api/users')
 
 app.use('/api/login', login)
 
@@ -18,4 +25,12 @@ const port = process.env.PORT || 5000
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
+
+    client.getBuyPrice({'currencyPair': 'BTC-USD'}, function(err, obj) {
+        console.log('total amount: ' + obj.data.amount);
+
+        if (err) {
+            console.error(err)
+        }
+    });
 })
